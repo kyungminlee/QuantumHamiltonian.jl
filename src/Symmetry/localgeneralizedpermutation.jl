e43f1135e52f99e0b57825d6c119395746e274de
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2021-06-10 00:04:19 +0900

    Fix LocalGeneralizedPermutation

--- src/Symmetry/localgeneralizedpermutation.jl
+++ src/Symmetry/localgeneralizedpermutation.jl
@@ -1,6 +1,8 @@
 export LocalGeneralizedPermutation
 
-struct LocalGeneralizedPermutation{A} <: AbstractSymmetryOperation
+using GroupTools
+
+struct LocalGeneralizedPermutation{A} <: GroupTools.AbstractSymmetryOperation
     operations::Vector{GeneralizedPermutation{A}}
     LocalGeneralizedPermutation(operations::AbstractVector{GeneralizedPermutation{A}}) where {A} = new{A}(operations)
 end
@@ -15,14 +17,19 @@ end
 
 Base.inv(arg::LocalGeneralizedPermutation) = LocalGeneralizedPermutation(inv.(arg.operations))
 
-function Base.:(*)(lhs::LocalGeneralizedPermutation, rhs::SitePermutation)
-    return DirectProductOperation(lhs, rhs)
-end
+Base.hash(arg::L, h::UInt) where {L<:LocalGeneralizedPermutation} = hash(L, hash(arg.operations, h))
 
-# Simpler version
-function Base.:(*)(lhs::SitePermutation, rhs::LocalGeneralizedPermutation)
-    return DirectProductOperation(lhs, rhs)
-end
+
+
+# NOTE! NOT DIRECT PRODUCT!!!
+# function Base.:(*)(lhs::LocalGeneralizedPermutation, rhs::SitePermutation)
+#     return DirectProductOperation(lhs, rhs)
+# end
+
+# # Simpler version
+# function Base.:(*)(lhs::SitePermutation, rhs::LocalGeneralizedPermutation)
+#     return DirectProductOperation(lhs, rhs)
+# end
 
 # # SitePermutation P: ψ[i] ↦ ψ[P(i)] = ψ'[i]
 # # LocalUnitary    U: ψ[i] ↦ U[i] * ψ[i] = ψ'[i]
@@ -37,4 +44,55 @@ end
 function Base.:(==)(lhs::LocalGeneralizedPermutation, rhs::LocalGeneralizedPermutation)
     return all(lhs.operations .== rhs.operations)
 end
+
+
+# function Base.:(*)(
+#     x::ProductOperation{<:Tuple{<:SitePermutation, <:LocalGeneralizedPermutation}},
+#     y::ProductOperation{<:Tuple{<:SitePermutation, <:LocalGeneralizedPermutation}}
+# )
+#     px, ux = x.operations
+#     py, uy = y.operations
+
+#     n = length(px.permutation.map)
+#     if n != length(py.permutation.map)
+#         throw(ArgumentError("the two operations should have the same number of sites"))
+#     end
+
+#     pz = px * py
+#     # inv_py = inv(py)
+#     uz = LocalGeneralizedPermutation([
+#         ux.operations[ py(i) ] * uy.operations[i]
+#         for i in 1:n
+#     ])
+#     return ProductOperation(pz, uz)
+# end
+
+
+
+function (permutation::SitePermutation)(op::LocalGeneralizedPermutation)
+    out = similar(op.operations)
+    for (i, x) in enumerate(op.operations)
+        out[permutation(i)] = x
+    end
+    return LocalGeneralizedPermutation(out)
+end
+
+
+
+
+# function Base.:(*)(
+#     x::ProductOperation{<:Tuple{<:SitePermutation, <:LocalGeneralizedPermutation}},
+#     y::ProductOperation{<:Tuple{<:SitePermutation, <:LocalGeneralizedPermutation}}
+# )
+#     px, ux = x.operations
+#     py, uy = y.operations
+
+#     n = length(px.permutation.map)
+#     if n != length(py.permutation.map)
+#         throw(ArgumentError("the two operations should have the same number of sites"))
+#     end
+#     pz = px * py
+#     uz = py(ux) * uy
+#     return ProductOperation(pz, uz)
+# end
 
