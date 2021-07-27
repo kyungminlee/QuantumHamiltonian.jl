2ff8197bd4b701e37c99c08c76f694a4780ed8d3
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2021-07-27 16:48:57 -0400

    Update message for deprecated symmetry_reduce

--- test/test_symmetry_apply.jl
+++ test/test_symmetry_apply.jl
@@ -5,7 +5,6 @@ using LatticeTools
 using QuantumHamiltonian.Toolkit: pauli_matrix
 
 @testset "symmetry_apply" begin
-
     unitcell = makeunitcell(1.0; SiteType=String)
     addsite!(unitcell, "Spin", FractCoord([0], [0.0]))
     lattice = make_lattice(unitcell, 4)
